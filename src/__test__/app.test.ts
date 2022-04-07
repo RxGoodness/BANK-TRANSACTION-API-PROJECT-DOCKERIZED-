5501c10966e2d45139f1e27dff5df462e2323587
Adedokun Toheeb Adetunji <63404340+RxNode@users.noreply.github.com>
2022-04-07 14:53:19 +0100

    heroku

--- src/__test__/app.test.ts
+++ src/__test__/app.test.ts
@@ -1,90 +1,105 @@
-import request from 'supertest';
-import app from '../app';
+import request from "supertest";
+import app from "../app";
 
 const balance = {
-  accountNo: 5910296934,
-  balance: 76000,
-  createdAt: '2022-04-06T15:17:57.931Z',
+  "accountNo": 3708871056,
+    "balance": 44000,
+    "createdAt": "2022-04-06T15:17:47.855Z"
 };
 
 const balancePost = {
-  balance: 76000,
+  "balance": 44000,
 };
 
 const transaction = {
-  reference: '6b9bc2fd-4e41-466e-bfed-0b33f73387ee',
-  senderAccount: 3717596176,
-  amount: 3000,
-  receiverAccount: 7337122585,
-  transferDescription: 'Payment',
-  createdAt: '2022-04-06T21:58:01.419Z',
-};
+  "reference": "553b8e0e-0310-4edb-933a-0dcb1332a76a",
+    "senderAccount": 3708871056,
+    "amount": 3000,
+    "receiverAccount": 5910296934,
+    "transferDescription": "Payment",
+    "createdAt": "2022-04-06T15:20:20.606Z"
+}
 
 const trans = {
-  senderAccount: 3717596176,
-  receiverAccount: 7337122585,
-  amount: 3000,
-  transferDescription: 'Payment',
-};
+  "senderAccount": 3708871056,
+  "amount": 2000,
+  "receiverAccount": 5910296934,
+  "transferDescription": "for payment",
+}
 
-describe('Should respond with 200 when user is posted', () => {
-  it('POST /send', async () => {
+
+// let accountNumber;
+
+describe("Should respond with 200 when user is posted", () => {
+
+  it("POST /send", async () =>  {
     const res = await request(app)
-      .post('/create')
-      // .expect("Content-Type")
+      .post("/create-account")
+      .expect("Content-Type", /json/)
       .send({
-        balancePost,
+        balancePost
       });
-    expect(200).toBe(200);
+      expect(200).toBe(200)
   });
 
-  it('should return 404 status code', async () => {
-    const res = await request(app).post('').send(balancePost);
+  it("should return 404 status code", async () => {
+    const res = await request(app)
+    .post("").send(balancePost);
     expect(404).toBe(404);
+  })
+
+});
+
+describe("Should return 200 for users found", () => {
+  it("GET /balance", async () => {
+    const res = await request(app).get("/balances")
+      .send({
+        balance
+      })
+      expect(200).toStrictEqual(200)
+  });
+
+  it("GET /", async () => {
+    const res = await request(app).get("/bal")
+      .send({
+        balance
+      })
+      expect(404).toBe(404)
   });
 });
 
-describe('Should return 200 for users found', () => {
-  it('GET /balance', async () => {
-    const res = await request(app).get('/balances').send({
-      balance,
+
+describe("GET /balances/:id", () => {
+
+    it("should return an 200 status for item found", async () => {
+      let accountNumber;
+        const res = await request(app).get(`/balances/${accountNumber}`);
+        expect(res.status).toBe(404);
     });
-    expect(200).toStrictEqual(200);
+
+});
+
+describe("GET /balances/:id", () => {
+
+  it("should return an 404 status for item not found", async () => {
+    let accountNumber;
+      const res = await request(app).put(`/${accountNumber}`);
+      expect(404).toBe(404);
   });
 
-  it('GET /', async () => {
-    const res = await request(app).get('/bal').send({
-      balance,
+});
+
+describe("POST /transfer", () => {
+
+    it("should return a status for item not found", async () => {
+        const res = await request(app).delete(`/transfer`);
+        expect(200).toBe(200);
     });
-    expect(404).toBe(404);
-  });
-});
 
-describe('GET /balances/:id', () => {
-  it('should return an 200 status for item found', async () => {
-    let accountNumber;
-    const res = await request(app).get(`/balances/${accountNumber}`);
-    expect(res.status).toBe(404);
-  });
-});
+    it("POST /transfer", async () => {
+      let accountNumber;
+      const res = await request(app).delete(`/transfer/${accountNumber}`)
+        expect(res.status).toBe(404);
+    });
 
-describe('GET /balances/:id', () => {
-  it('should return an 404 status for item not found', async () => {
-    let accountNumber;
-    const res = await request(app).put(`/${accountNumber}`);
-    expect(404).toBe(404);
-  });
-});
-
-describe('POST /transfer', () => {
-  it('should return a status for item not found', async () => {
-    const res = await request(app).get(`/transfer`);
-    expect(200).toBe(200);
-  });
-
-  it('POST /transfer', async () => {
-    let accountNumber;
-    const res = await request(app).get(`/transfer/${accountNumber}`);
-    expect(res.status).toBe(404);
-  });
 });
